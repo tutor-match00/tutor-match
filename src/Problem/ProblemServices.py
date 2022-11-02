474d47c1f9e2b075b232e624428874b84cfdcd79
trevenue44 <trevenue44.main@gmail.com>
2022-11-02 22:27:31 +0000

    endpoint to get all problems

--- src/Problem/ProblemServices.py
+++ src/Problem/ProblemServices.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,request
+from flask import Blueprint, request
 from Problem.ProblemModel import Problem
 from Tutor.TutorModel import Tutor
 
@@ -12,72 +12,86 @@ def get_all_problems():
     from app import session
     try:
         problems = session.query(Problem).all()
-        print(problems)
     except:
         pass
-        
+
     return {
-        'problems': "Hello World"
+        "status": True,
+        "msg": [{
+            "id_problem": problem.id,
+            "title": problem.title,
+            "description": problem.description,
+            "course_tag": problem.course_tag,
+            "status": problem.status,
+            "tutee_id": problem.tutee_id,
+            "tag_id": problem.tag_id,
+            "tutor_id_tutor": problem.tutor_id_tutor
+
+        } for problem in problems]
     }
+
 
 @problem_routes.route('/problem/<tagId>', methods=['GET'])
 def tutorMatch(tagId):
     from app import session
     try:
-        problem_match = session.query(Problem).filter(Problem.status==1 and Problem.tag_id == tagId).first()
-        tutor = session.query(Tutor).filter(Tutor.tag_id == tagId and Tutor.status == 1).first()
-        if(tutor):
+        problem_match = session.query(Problem).filter(
+            Problem.status == 1 and Problem.tag_id == tagId).first()
+        tutor = session.query(Tutor).filter(
+            Tutor.tag_id == tagId and Tutor.status == 1).first()
+        if (tutor):
             return ({
-            "status": True,
-            "msg": {
-                "id_problem": problem_match.id,
-                "title": problem_match.title,
-                "description": problem_match.description,
-                "tutor_name": tutor.first_name + tutor.last_name,
-                "tutor_whatsapp": tutor.whatsapp_number
-            }            
-            }),200
+                "status": True,
+                "msg": {
+                    "id_problem": problem_match.id,
+                    "title": problem_match.title,
+                    "description": problem_match.description,
+                    "tutor_name": tutor.first_name + tutor.last_name,
+                    "tutor_whatsapp": tutor.whatsapp_number
+                }
+            }), 200
         else:
             return ({
-            "status": False,
-            "msg": "No tutor available"      
-            }),200
-        
+                "status": False,
+                "msg": "No tutor available"
+            }), 200
+
     except Exception as e:
-        return  ({
-                 'status': False,
-                 'msg':{
-                        "dev_message" : (f"{e}"),
-                        "message":"Error"
-                        }
-                }),400
-        
+        return ({
+            'status': False,
+            'msg': {
+                "dev_message": (f"{e}"),
+                "message": "Error"
+            }
+        }), 400
 
 
 @problem_routes.route('/problem/<tuteeId>', methods=['GET'])
 def tuteeProblems(tuteeId):
     from app import session
     try:
-        problems = session.query(Problem).filter(Problem.tutee_id == tuteeId ).all()
+        problems = session.query(Problem).filter(
+            Problem.tutee_id == tuteeId).all()
         print(problems)
         return {
             'status': True,
-            "msg" : [{
+            "msg": [{
                 "id_problem": problem.id,
                 "title": problem.title,
                 "description": problem.description,
             } for problem in problems
             ]}
     except Exception as e:
-                session.rollback()  
-                return  ( {
-                'msg': {
-                    "message": "Connection Error: Unable to register hospital",
-                    "dev_message": (f"{e}"),
-                    
-                },
-                "status": False
-            }),400
+        session.rollback()
+        return ({
+            'msg': {
+                "message": "Connection Error: Unable to register hospital",
+                "dev_message": (f"{e}"),
+
+            },
+            "status": False
+        }), 400
+
 
 @problem_routes.route('/problems/', methods=['POST'])
 def enterProblem():
@@ -96,19 +110,19 @@ def enterProblem():
             course = request_data["course"]
             topic = request_data["topic"]
             problem_description = request_data["problem_description"]
-            try: 
+            try:
                 session.add(newHospital)
                 session.commit()
             except Exception as e:
-                session.rollback()  
-                return  ( {
-                'msg': {
-                    "message": "Connection Error: Unable to register hospital",
-                    "dev_message": (f"{e}"),
-                    
-                },
-                "status": False
-            }),400
+                session.rollback()
+                return ({
+                    'msg': {
+                        "message": "Connection Error: Unable to register hospital",
+                        "dev_message": (f"{e}"),
+
+                    },
+                    "status": False
+                }), 400
         except Exception as e:
             return ({
                 "status": False,
